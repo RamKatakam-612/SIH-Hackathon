d4909c8978577f70d1ffed35a6d90dbcb23c4816
RamKatakam-612 <mr.rockyram666@gmail.com>
2025-09-14 22:20:56 +0530

    Update Sentimental_Analysis.py

--- SIH_TEAM_MUFASA/E_consultation_module_SA/Sentimental_Analysis.py
+++ SIH_TEAM_MUFASA/E_consultation_module_SA/Sentimental_Analysis.py
@@ -124,7 +124,7 @@ if st.session_state.page == "Analysis":
 
         try:
             import os
-            base_path = os.path.dirname(file)
+            base_path = os.path.dirname(__file__)
             csv_path = os.path.join(base_path, "imdb_sample.csv")
             imdb_df = pd.read_csv(csv_path)
             #imdb_df = pd.read_csv("imdb_sample.csv")
@@ -543,4 +543,5 @@ elif st.session_state.page == "About":
         </ul>
     </div>
     """, unsafe_allow_html=True)
+
 
